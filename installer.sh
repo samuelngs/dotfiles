7efc11f58a74c16232be3f4cbf9fa280e96ca5ae
Samuel <sam@infinitely.io>
2015-12-14 02:48:02 -0700

    auto install

--- installer.sh
+++ installer.sh
@@ -2,14 +2,22 @@
 
 REQUIRE_LIBS_APT_NEOVIM="python python-dev python-pip"
 REQUIRE_LIBS_PIP_NEOVIM="neovim"
+
 NEOVIM_PATH="$HOME/.config/nvim"
 OLDVIM_PATH="$HOME/.vim"
 
 VUNDLE_PATH="$NEOVIM_PATH/bundle/"
 
-REQUIRE_LIBS_APT_YCM="libclang-dev gocode libclang1-3.6 libjs-sphinxdoc libobjc-5-dev libobjc4 libruby2.1 libyaml-0-2 python-bottle python-concurrent.futures python-jedi python-requests-futures python-waitress ruby ruby2.1 rubygems-integration libboost-all-dev"
+REQUIRE_LIBS_APT_YCM="libclang-dev gocode libclang1-3.6 libjs-sphinxdoc libobjc-5-dev libobjc4 libruby2.1 libyaml-0-2 python-bottle python-concurrent.futures python-jedi python-requests-futures python-waitress ruby ruby2.1 rubygems-integration libboost-all-dev npm"
 
-REQUIRE_LIBS_APT_DUNST=""
+apt-get install --yes $REQUIRE_LIBS_APT_NEOVIM
+pip install $REQUIRE_LIBS_PIP_NEOVIM
+
+apt-get install --yes $REQUIRE_LIBS_APT_YCM
+
+npm install -g n
+n stable
+
+npm install -g typescript
 
 ./install.py --system-libclang --system-boost --clang-completer --omnisharp-completer --gocode-completer
-npm install -g typescript
